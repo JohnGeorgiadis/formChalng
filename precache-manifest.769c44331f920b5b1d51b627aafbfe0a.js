2f13e8aa223df6fe8b6c963c740952517fcc1ced
Ioannis Georgiadis <j.georgiadiss@gmail.com>
2020-10-12 16:35:27 +0200

    Updates

---
+++ precache-manifest.769c44331f920b5b1d51b627aafbfe0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b15040311a6dc5fc31ddfcafeeb90f8f",
+    "url": "/formChalng/index.html"
+  },
+  {
+    "revision": "7b1c1a92423012bd844a",
+    "url": "/formChalng/static/css/main.f04be1d2.chunk.css"
+  },
+  {
+    "revision": "ffd9c29b2490a96b36b8",
+    "url": "/formChalng/static/js/2.09d4e5d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/formChalng/static/js/2.09d4e5d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b1c1a92423012bd844a",
+    "url": "/formChalng/static/js/main.3fb01d73.chunk.js"
+  },
+  {
+    "revision": "9a3ea90b31bd55e601ca",
+    "url": "/formChalng/static/js/runtime-main.f712f954.js"
+  }
+]);
